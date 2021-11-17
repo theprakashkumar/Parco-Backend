5965f98fbbcb3cd1f39360da7a5f83287b2776d2
PRAKASH <38276550+ThePrakashKumar@users.noreply.github.com>
2021-11-17 14:38:51 +0530

    refactor: refactore like controller

--- controllers/like.controllers.js
+++ controllers/like.controllers.js
@@ -12,10 +12,10 @@ const likePost = async (req, res) => {
             post.likes.push(userId);
             const updatedPost = await post.save();
 
-            console.log("post", post);
             return res.status(200).json({
                 success: true,
-                post: updatedPost,
+                postId: updatedPost._id,
+                likes: updatedPost.likes,
             });
         }
         return res.status(401).json({
@@ -23,6 +23,7 @@ const likePost = async (req, res) => {
             message: "Post not Found!",
         });
     } catch (error) {
+        console.log(error);
         return res.status(400).json({
             status: false,
             message: "Can't Like the Post!",
@@ -40,10 +41,11 @@ const removeLike = async (req, res) => {
 
         if (post) {
             post.likes.splice(post.likes.indexOf(userId), 1);
-            const updatePost = await post.save();
+            const updatedPost = await post.save();
             return res.status(200).json({
                 success: true,
-                post: updatePost,
+                postId: updatedPost._id,
+                likes: updatedPost.likes,
             });
         }
 
@@ -52,6 +54,7 @@ const removeLike = async (req, res) => {
             message: "Post not Found!",
         });
     } catch (error) {
+        console.log(error);
         return res.status(400).json({
             status: false,
             message: "Can't Like the Post!",
